90fceef677abbf4752915d9510c6927dc97cf1e0
J.H. JIN <git.ghbt@gmail.com>
2025-11-10 23:51:44 +0900

    chore(init): initialize project

---
+++ src/main/java/in/logicalerror/authadminapi/AuthAdminApiApplication.java
@@ -0,0 +1,13 @@
+package in.logicalerror.authadminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthAdminApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/logicalerror/authadminapi/AuthAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.logicalerror.authadminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
