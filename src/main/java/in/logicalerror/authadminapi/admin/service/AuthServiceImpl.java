f10c82eca280aaced34b0f9916d3a10349556de8
J.H. JIN <git.ghbt@gmail.com>
2025-11-21 16:11:37 +0900

    refactor(auth): apply read-only transaction to service
    - AuthServiceImpl 클래스에 @Transactional(readOnly = true) 적용

--- src/main/java/in/logicalerror/authadminapi/admin/service/AuthServiceImpl.java
+++ src/main/java/in/logicalerror/authadminapi/admin/service/AuthServiceImpl.java
@@ -6,10 +6,12 @@ import in.logicalerror.authadminapi.admin.domain.AuthRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AuthServiceImpl implements AuthService{
 
     private final AuthRepository authRepository;
